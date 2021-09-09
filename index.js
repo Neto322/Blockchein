7fdae6dc1d3cf77e101121c963322145cac55e3b
Neto322 <manuel123317@hotmail.com>
2021-09-08 19:55:18 -0700

    sd
    as

---
+++ index.js
@@ -0,0 +1,11 @@
+import PKG from './package.json';
+
+import Block from './src/blockchain/block';
+
+const { name, version, description, author } = PKG;
+
+console.log(`${name} ${version} ${description} ${author}`);
+
+const block = new Block(Date.now(), 'p3krk5', 'o6y', 'd4t4');
+
+console.log(block.toString());
